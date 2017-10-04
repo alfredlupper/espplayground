e2ff7c15d60441efb289e036f334fbbc5c5d5db4
Alfred Lupper <fred@lupper.de>
2017-10-04 16:30:09 +0200

    Update newconnect.cc

--- newconnect.cc
+++ newconnect.cc
@@ -1,6 +1,33 @@
-      pinMode(LEDpin, OUTPUT);
-      digitalWrite(LEDpin, 0);  // LED ON für 5 Sekunden
-      delay(5000);
-      digitalWrite(LEDpin, 1);
+
       
       
+bool Ubidots::wifiConnection(char* ssid, char* pass) {
+
+WiFi.begin(ssid, pass);
+
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+}
+      
+IF (WiFi.status() = WL_CONNECTED) {
+      WiFi.setAutoReconnect(true);
+      Serial.println(F("WiFi connected"));
+      Serial.println(F("IP address: "));
+      Serial.println(WiFi.localIP());
+      
+      pinMode(greenLEDpin, OUTPUT);
+      digitalWrite(greenLEDpin, 0);  // green LED ON für 2 Sekunden
+      delay(2000);
+      digitalWrite(greenLEDpin, 1);
+}      
+else {
+      Serial.println(F("WiFi failed to connect"));
+      pinMode(redLEDpin, OUTPUT);
+      digitalWrite(redLEDpin, 0);  // red LED ON für 2 Sekunden
+      delay(2000);
+      digitalWrite(redLEDpin, 1);
+      
+}
+
+}
